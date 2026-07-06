0d2caa74d270ec2718ec03d51dcd2dff20975bee
jvm2000 <minozajohnvincent2000@gmail.com>
2026-07-06 20:17:45 +0800

    Enhance layout by adjusting z-index for gradient background and adding a new gradient overlay

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@ import { HeartIcon } from '@heroicons/vue/24/solid'
     <div class="w-full h-[48rem] relative pb-8">
       <img src="/couch.png" alt="Couch Header" class="w-full h-full absolute object-cover z-0" />
 
-      <div class="left-0 absolute w-full grid grid-cols-5 h-[48rem] bg-[linear-gradient(to_right,#f3eae4_0%,#f3eae4_35%,rgba(243,234,228,.9)_45%,rgba(243,234,228,.6)_70%,rgba(243,234,228,.3)_75%,transparent_100%)]">
+      <div class="left-0 absolute w-full grid grid-cols-5 h-[48rem] bg-[linear-gradient(to_right,#f3eae4_0%,#f3eae4_35%,rgba(243,234,228,.9)_45%,rgba(243,234,228,.6)_70%,rgba(243,234,228,.3)_75%,transparent_100%)] z-10">
         <div class="col-span-2 px-24 flex flex-col items-center w-full">
           <div class="flex flex-col items-center space-y-1.5">
             <img src="/logo-header.png" alt="Logo Header" class="w-auto h-64" />
@@ -52,6 +52,8 @@ import { HeartIcon } from '@heroicons/vue/24/solid'
  
         <div class="col-span-3"></div>
       </div>
+
+      <div class="left-0 top-0 bg-[linear-gradient(to_bottom,#f3eae4_10%,#f3eae4_1%,transparent_100%)] w-full h-36 absolute z-0"></div>
     </div>
 
     <div class="px-28 py-8 flex flex-col items-center w-full bg-[#eee6e1] space-y-6">
